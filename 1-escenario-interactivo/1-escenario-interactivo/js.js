799b80a032ea1537a8cf8cb4cd0f06e4b0390ce9
Luna Rivera <lunanicolle31@gmail.com>
2025-10-21 15:24:52 -0500

    Add files via upload

--- 1-escenario-interactivo/1-escenario-interactivo/js.js
+++ 1-escenario-interactivo/1-escenario-interactivo/js.js
@@ -1,1 +1,25 @@
-const monedas = document.querySelectorAll(".monedi")
+const mone = document.querySelectorAll(".monedi")
+const contador = document.querySelector(".contador")
+
+console.log(mone)
+console.log(contador)
+let sumaDeContador = 0
+
+
+mone.forEach(moned => {
+    /*  console.log("elemento: ", moned) */
+    moned.addEventListener("click", () => {
+        moned.classList.add("saltar")
+        
+    sumaDeContador++
+    
+    contador.textContent = `SCORE ${sumaDeContador}`
+
+    })
+
+   
+
+    moned.addEventListener("animationend", () => {
+        moned.style.display = "none"
+    }, { once: true })
+})  /*  ejecute una vez y no vuelva a aparecer mas */
