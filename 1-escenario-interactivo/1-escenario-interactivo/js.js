ac08a096bfffbb83eec04c8a42873802544f96d1
Luna Rivera <lunanicolle31@gmail.com>
2025-10-24 19:50:26 -0500

    Add files via upload

--- 1-escenario-interactivo/1-escenario-interactivo/js.js
+++ 1-escenario-interactivo/1-escenario-interactivo/js.js
@@ -9,7 +9,7 @@ let sumaDeContador = 0
 mone.forEach(moned => {
     /*  console.log("elemento: ", moned) */
     moned.addEventListener("click", () => {
-        moned.classList.add("saltar")
+    moned.classList.add("saltar")
         
     sumaDeContador++
     
@@ -17,9 +17,43 @@ mone.forEach(moned => {
 
     })
 
-   
-
     moned.addEventListener("animationend", () => {
         moned.style.display = "none"
     }, { once: true })
 })  /*  ejecute una vez y no vuelva a aparecer mas */
+
+
+/////////////////////////////////////////////////////////////
+
+
+const estrellas = document.querySelectorAll(".estrella");
+const contador2 = document.querySelector(".contador2");
+let score2 = 0;
+
+estrellas.forEach(star => {
+  star.addEventListener("click", () => {
+    star.classList.add("saltar");
+    score2++;
+    contador2.textContent = `SCORE ${score2}`;
+  });
+
+  star.addEventListener("animationend", () => {
+    star.style.display = "none";
+  }, { once: true });
+});
+
+const objetos = document.querySelectorAll(".caja");
+const contador3 = document.querySelector(".contador3");
+let score3 = 0;
+
+objetos.forEach(obj => {
+  obj.addEventListener("click", () => {
+    obj.classList.add("saltar");
+    score3++;
+    contador3.textContent = `SCORE ${score3}`;
+  });
+
+  obj.addEventListener("animationend", () => {
+    obj.style.display = "none";
+  }, { once: true });
+});
