4375fd5051cc5fb8f970c05893fcd88cb2b7a685
Luna Rivera <lunanicolle31@gmail.com>
2025-10-31 15:56:51 -0500

    Add files via upload
    Función mostrar escena

---
+++ 1-escenario-interactivo/1-escenario-interactivo/js.js
@@ -0,0 +1,88 @@
+const mone = document.querySelectorAll(".monedi")
+const contador = document.querySelector(".contador")
+
+console.log(mone)
+console.log(contador)
+let sumaDeContador = 0
+
+
+mone.forEach(moned => {
+    /*  console.log("elemento: ", moned) */
+    moned.addEventListener("click", () => {
+    moned.classList.add("saltar")
+        
+    sumaDeContador++
+    
+    contador.textContent = `SCORE ${sumaDeContador}`
+
+    })
+
+    moned.addEventListener("animationend", () => {
+        moned.style.display = "none"
+    }, { once: true })
+})  /*  ejecute una vez y no vuelva a aparecer mas */
+
+
+/////////////////////////////////////////////////////////////
+
+
+const estrellas = document.querySelectorAll(".estrella");
+const contador2 = document.querySelector(".contador2");
+let score2 = 0;
+
+estrellas.forEach(star => {
+  star.addEventListener("click", () => {
+    star.classList.add("saltar");
+    score2++;
+    contador2.textContent = `SCORE ${score2}`;
+  });
+
+  star.addEventListener("animationend", () => {
+    star.style.display = "none";
+  }, { once: true });
+});
+
+const objetos = document.querySelectorAll(".caja");
+const contador3 = document.querySelector(".contador3");
+let score3 = 0;
+
+objetos.forEach(obj => {
+  obj.addEventListener("click", () => {
+    obj.classList.add("saltar");
+    score3++;
+    contador3.textContent = `SCORE ${score3}`;
+  });
+
+  obj.addEventListener("animationend", () => {
+    obj.style.display = "none";
+  }, { once: true });
+});
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/* Carrusel */
+
+//1.Variables
+const escenas = document.querySelectorAll(".escena")
+const btnAnterior = document.querySelector (".btn-anterior")
+const btnSiguiente = document.querySelector (".btn-siguiente")
+const miniaturas = document.querySelectorAll (".miniatura")
+let indice = 0
+
+console.log (escenas)
+console.log (btnAnterior)
+console.log (btnSiguiente)
+console.log (miniaturas)
+
+//2.funciones
+function mostrarEscena(i) {
+  for (let j = 0; j < escenas.length; j++){
+    escenas [j].classList.remove('activa')
+  }
+
+  escenas [i].classList.add('activa')
+}
+
+mostrarEscena (2)
+//3.Eventos
