500752c0d0bbbbc4e9e3681e11c1c164da946c41
Luna Rivera <lunanicolle31@gmail.com>
2025-11-05 15:30:25 -0500

    Add files via upload
    Sonidos

--- 1-escenario-interactivo/js.js
+++ 1-escenario-interactivo/js.js
@@ -1,10 +1,37 @@
 const mone = document.querySelectorAll(".monedi")
 const contador = document.querySelector(".contador")
+const sonidoMoneda = document.querySelector("#sonido-moneda");
+const sonidoEstrella = document.querySelector("#sonido-estrella");
+const sonidoCaja = document.querySelector("#sonido-caja");
+const sonidoVoz = document.querySelector("#sonido-voz")
+const perso = document.querySelector(".perso");
+const prince = document.querySelector(".prince");
 
 console.log(mone)
 console.log(contador)
 let sumaDeContador = 0
 
+
+perso.addEventListener("click", () => {
+
+  perso.classList.add("salto");
+  perso.addEventListener("animationend", () => {
+    perso.classList.remove("salto");
+
+    sonidoVoz.pause();
+    sonidoVoz.currentTime = 0;
+    sonidoVoz.play();
+  }, { once: true });
+});
+
+
+
+prince.addEventListener("click", () => {
+  prince.classList.add("salto");
+  prince.addEventListener("animationend", () => {
+    prince.classList.remove("salto");
+  }, { once: true });
+});
 
 mone.forEach(moned => {
   /*  console.log("elemento: ", moned) */
@@ -15,6 +42,9 @@ mone.forEach(moned => {
 
     contador.textContent = `SCORE ${sumaDeContador}`
 
+    sonidoMoneda.pause();
+    sonidoMoneda.currentTime = 0;
+    sonidoMoneda.play();
   })
 
   moned.addEventListener("animationend", () => {
@@ -35,7 +65,13 @@ estrellas.forEach(star => {
     star.classList.add("saltar");
     score2++;
     contador2.textContent = `SCORE ${score2}`;
+
+    sonidoEstrella.pause();
+    sonidoEstrella.currentTime = 0;
+    sonidoEstrella.play();
   });
+
+
 
   star.addEventListener("animationend", () => {
     star.style.display = "none";
@@ -51,7 +87,13 @@ objetos.forEach(obj => {
     obj.classList.add("saltar");
     score3++;
     contador3.textContent = `SCORE ${score3}`;
+
+    sonidoCaja.pause();
+    sonidoCaja.currentTime = 0;
+    sonidoCaja.play();
   });
+
+
 
   obj.addEventListener("animationend", () => {
     obj.style.display = "none";
@@ -91,28 +133,28 @@ mostrarEscena(2)
 //funcion siguiente
 btnSiguiente.addEventListener("click", () => {
   indice = indice - 1
-  if (indice < 0){
+  if (indice < 0) {
     indice = escenas.length - 1 //para ir a la ultima img
   }
   mostrarEscena(indice)
- 
+
 })
 
 //funcion anterior
 btnAnterior.addEventListener("click", () => {
   indice = indice + 1
-  if (indice = escenas.length - 1){
+  if (indice = escenas.length - 1) {
     indice = 0
   }
   mostrarEscena(indice)
 })
 
 //funcion miniatura
-miniaturas.forEach ( (min, i) =>  {
+miniaturas.forEach((min, i) => {
   min.addEventListener("click", () => {
     mostrarEscena(i)
-  
+
   })
-  
+
 })
 //3.Eventos
