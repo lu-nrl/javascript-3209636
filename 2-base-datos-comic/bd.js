cf772df060fa4a02f3f645345cb0b67ebaefb873
Luna Rivera <lunanicolle31@gmail.com>
2025-11-06 15:20:41 -0500

    Add files via upload
    Base de datos

---
+++ 2-base-datos-comic/bd.js
@@ -0,0 +1,119 @@
+let comic = {
+    "nombreComic": "Cenizas De Honor",
+    "sipnosis": "Un caballero siguiendo su destino",
+    "year": 2025,
+    "autores": ["Luna", "Sofia", "Alexis", "Miguel"],
+    "personajes": ["Sr. Leon", "Elvira", "Ninove", "Brujas del alba", "Rey", "Reina", "Consejero", "Caballeros", "Cristobal Colón"],
+    "genero": ["Historia", "Fantasia", "Ciencia Ficción", "Acción"],
+    "portada": "./imagenes/portada.jpg",
+    "escenas": [
+        {
+            "id": 1,
+            "nombre": "Molecule Man",
+            "personajes": ["Personaje1", "Personaje2"],
+            "image": "./images/portada.png",
+        },
+        {
+             "id": 2,
+            "nombre": "Molecule Man",
+            "personajes": ["Personaje1", "Personaje2"],
+            "image": "./images/portada.png",
+        },
+        {
+            "id": 3,
+            "nombre": "Molecule Man",
+            "personajes": ["Personaje1", "Personaje2"],
+            "image": "./images/portada.png",
+        },
+        {
+            "id": 4,
+            "nombre": "Molecule Man",
+            "personajes": ["Personaje1", "Personaje2"],
+            "image": "./images/portada.png",
+        },
+        {
+            "id": 5,
+            "nombre": "Molecule Man",
+            "personajes": ["Personaje1", "Personaje2"],
+            "image": "./images/portada.png",
+        },
+        {
+            "id": 6,
+            "nombre": "Molecule Man",
+            "personajes": ["Personaje1", "Personaje2"],
+            "image": "./images/portada.png",
+        },
+        {
+            "id": 7,
+            "nombre": "Molecule Man",
+            "personajes": ["Personaje1", "Personaje2"],
+            "image": "./images/portada.png",
+        },
+        {
+            "id": 8,
+            "nombre": "Molecule Man",
+            "personajes": ["Personaje1", "Personaje2"],
+            "image": "./images/portada.png",
+        },
+        {
+            "id": 9,
+            "nombre": "Molecule Man",
+            "personajes": ["Personaje1", "Personaje2"],
+            "image": "./images/portada.png",
+        },
+        {
+            "id": 10,
+            "nombre": "Molecule Man",
+            "personajes": ["Personaje1", "Personaje2"],
+            "image": "./images/portada.png",
+        },
+        {
+            "id": 11,
+            "nombre": "Molecule Man",
+            "personajes": ["Personaje1", "Personaje2"],
+            "image": "./images/portada.png",
+        },
+        {
+            "id": 12,
+            "nombre": "Molecule Man",
+            "personajes": ["Personaje1", "Personaje2"],
+            "image": "./images/portada.png",
+        },
+        {
+            "id": 13,
+            "nombre": "Molecule Man",
+            "personajes": ["Personaje1", "Personaje2"],
+            "image": "./images/portada.png",
+        },
+        {
+            "id": 14,
+            "nombre": "Molecule Man",
+            "personajes": ["Personaje1", "Personaje2"],
+            "image": "./images/portada.png",
+        },
+        {
+            "id": 15,
+            "nombre": "Molecule Man",
+            "personajes": ["Personaje1", "Personaje2"],
+            "image": "./images/portada.png",
+        },
+        {
+            "id": 16,
+            "nombre": "Molecule Man",
+            "personajes": ["Personaje1", "Personaje2"],
+            "image": "./images/portada.png",
+        },
+        {
+            "id": 17,
+            "nombre": "Molecule Man",
+            "personajes": ["Personaje1", "Personaje2"],
+            "image": "./images/portada.png",
+        },
+        {
+            "id": 18,
+            "nombre": "Molecule Man",
+            "personajes": ["Personaje1", "Personaje2"],
+            "image": "./images/portada.png",
+        }
+    ]
+}
