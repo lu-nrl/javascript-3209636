cb0f8a0c49f1ac5a0991ac4f4d853ad9894f209b
Luna Rivera <lunanicolle31@gmail.com>
2025-11-16 21:21:40 -0500

    Add files via upload

--- 2-base-datos-comic/app.js
+++ 2-base-datos-comic/app.js
@@ -1,15 +1,1 @@
-const infoComic = document.querySelector(".info-comic")
-const listaCapi = document.querySelector(".lista-capitulos")
-
-console.log(infoComic)
-console.log(listaCapi)
-
-//Modificar el contenido de info general del comic
-
-infoComic.innerHTML = `
-        <p> ${comic.year} </p>
-        <h1>${ comic.nombreComic}</h1>
-        <p> ${comic.sinopsis} </p>
-        <p> <b> Genero: </b>  ${comic.genero.join (", ")}</p>
-        <p><b> personajes: </b> ${comic.personajes.join (", ")}
-`
+ÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿ
--- 2-base-datos-comic/index.js
+++ 2-base-datos-comic/index.js
@@ -1,15 +1,7 @@
-const Princi = document.querySelector(".principal")
-const Marca = document.querySelector(".texto-marca")
-const Comic = document.querySelector(".comic")
-
-console.log(Princi)
-console.log(Marca)
-console.log(Comic)
-
-Marca.innerHTML = `
-        <p> ${comic.year} </p>
-        <h1>${ comic.nombreComic}</h1>
-        <p> ${comic.sinopsis} </p>
-        <p> <b> Genero: </b>  ${comic.genero.join (", ")}</p>
-        <p><b> personajes: </b> ${comic.personajes.join (", ")}
-`
+document.querySelector(".info-comic").innerHTML = `
+    <b>Año:</b> ${comic.year}<br>
+    <b>Título:</b> ${comic.nombreComic}<br>
+    <b>Sinopsis:</b> ${comic.sinopsis}<br>
+    <b>Géneros:</b> ${comic.genero.join(", ")}<br>
+    <b>Autores:</b> ${comic.autores.join(", ")}
+`;
