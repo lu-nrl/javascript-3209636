8c068d4af9cfa2dd0af322c10df1f3949f52e3fa
Luna Rivera <lunanicolle31@gmail.com>
2025-11-24 15:42:36 -0500

    Add files via upload
    Otra pagina

---
+++ 2-base-datos-comic/pagina principal/bd.js
@@ -0,0 +1,186 @@
+let comic = {
+    "nombreComic": "Cenizas De Honor",
+    "sinopsis": "Un caballero siguiendo su destino",
+    "year": 2025,
+    "autores": ["Luna", "Sofia", "Alexis", "Miguel"],
+
+    "personajes":[
+        {
+          "nombre": "Sr.León",
+          "imagen": "./Img/Sr.León.jpg",
+        },
+        {
+        "nombre": "Elvira",
+        "imagen": "./Img/Elvira.jpg",
+        },
+        {
+        "nombre": "Rey",
+        "imagen": "./Img/Rey.jpg",
+        },{
+        "nombre": "Reina",
+        "imagen": "./Img/Reina.jpg",
+        },{
+        "nombre": "Sirvienta",
+        "imagen": "./Img/Sirvienta.jpg",
+        },{
+        "nombre": "Brujas del alba",
+        "imagen": "./Img/brujas.jpg",
+        },{
+        "nombre": "Pueblo",
+        "imagen": "./Img/Gente.jpg",
+        },{
+        "nombre": "Cristobal Colón",
+        "imagen": "./Img/Cristobal.jpg",
+        },
+        {
+        "nombre": "Caballeros",
+        "imagen": "./Img/Caballeros.jpg",
+        },
+        {
+        "nombre": "Ninove",
+        "imagen": "./Img/Ninove.jpg",
+        },
+        {
+        "nombre": "Consejero",
+        "imagen": "./Img/Consejero.jpg",
+        },
+    ],
+
+    "capitulos": [
+        {
+          "nombre1": "CAP 1: PRIMERAS CIVILIZACIONES Y LA INVENCION DE LA ESCRITURA",
+          "imagen1": "./Img/primeras\ civilizaciones.jpg",
+        },
+        {
+          "nombre2": "CAP 2: LA ANTIGUA GRECIA Y ROMA",
+          "imagen2": "./Img/antigua\ grecia\ y\ roma.jpg",
+        },
+        {
+          "nombre2": "CAP 3: LA EDAD MEDIA Y LA GESTION DEL CONOCIMIENTO Y LA INNOVACION",
+          "imagen2": "./Img/Edad\ media\ gestion\ de\ conocimieto.jpg",
+        },
+        {
+          "nombre2": "CAP 4: REVOLUCION INDUSTRIAL",
+          "imagen2": "./Img/revolucion\ industrial.jpg",
+        },
+        {
+          "nombre2": "CAP 5: ERA DIGITAL",
+          "imagen2": "./Img/era\ digital.jpg",
+        },
+    ],
+
+
+
+    "genero": ["Historia", "Fantasia", "Ciencia Ficción", "Acción"],
+    "portada": "./Img/Imagen1.png",
+    "escenas": [
+        {
+            "id": 1,
+            "nombre": "ghdfhawgeheage",
+            "personajes": ["elvi", "reina"],
+            "image": "./Img/batalla bosque.jpg",
+        },
+        {
+             "id": 2,
+            "nombre": "",
+            "personajes": ["Personaje1", "Personaje2"],
+            "image": "./Img/castillo-rey.jpg",
+        },
+        {
+            "id": 3,
+            "nombre": "",
+            "personajes": ["Personaje1", "Personaje2"],
+            "image": "./img/.png",
+        },
+        {
+            "id": 4,
+            "nombre": "",
+            "personajes": ["Personaje1", "Personaje2"],
+            "image": "./img/.png",
+        },
+        {
+            "id": 5,
+            "nombre": "",
+            "personajes": ["Personaje1", "Personaje2"],
+            "image": "./img/.png",
+        },
+        {
+            "id": 6,
+            "nombre": "",
+            "personajes": ["Personaje1", "Personaje2"],
+            "image": "./img/.png",
+        },
+        {
+            "id": 7,
+            "nombre": "",
+            "personajes": ["Personaje1", "Personaje2"],
+            "image": "./img/.png",
+        },
+        {
+            "id": 8,
+            "nombre": "",
+            "personajes": ["Personaje1", "Personaje2"],
+            "image": "./img/.png",
+        },
+        {
+            "id": 9,
+            "nombre": "",
+            "personajes": ["Personaje1", "Personaje2"],
+            "image": "./img/.png",
+        },
+        {
+            "id": 10,
+            "nombre": "",
+            "personajes": ["Personaje1", "Personaje2"],
+            "image": "./img/.png",
+        },
+        {
+            "id": 11,
+            "nombre": "",
+            "personajes": ["Personaje1", "Personaje2"],
+            "image": "./img/.png",
+        },
+        {
+            "id": 12,
+            "nombre": "",
+            "personajes": ["Personaje1", "Personaje2"],
+            "image": "./img/.png",
+        },
+        {
+            "id": 13,
+            "nombre": "",
+            "personajes": ["Personaje1", "Personaje2"],
+            "image": "./images/.png",
+        },
+        {
+            "id": 14,
+            "nombre": "",
+            "personajes": ["Personaje1", "Personaje2"],
+            "image": "./images/.png",
+        },
+        {
+            "id": 15,
+            "nombre": "",
+            "personajes": ["Personaje1", "Personaje2"],
+            "image": "./images/.png",
+        },
+        {
+            "id": 16,
+            "nombre": "",
+            "personajes": ["Personaje1", "Personaje2"],
+            "image": "./images/.png",
+        },
+        {
+            "id": 17,
+            "nombre": "",
+            "personajes": ["Personaje1", "Personaje2"],
+            "image": "./images/.png",
+        },
+        {
+            "id": 18,
+            "nombre": "",
+            "personajes": ["Personaje1", "Personaje2"],
+            "image": "./images/.png",
+        }
+    ]
+}
---
+++ 2-base-datos-comic/pagina principal/index.js
@@ -0,0 +1,56 @@
+const listaEscenas = document.querySelector (".escenas-lista")
+const personajesContainer= document.querySelector (".personajes-lista")
+const infoComic= document.querySelector (".personajes-lista")
+/* const capitulos= document.querySelector (".") */
+const Comic= document.querySelector(".info-comic").innerHTML = `
+    <b>Año:</b> ${comic.year}<br>
+    <b>Título:</b> ${comic.nombreComic}<br>
+    <b>Sinopsis:</b> ${comic.sinopsis}<br>
+    <b>Géneros:</b> ${comic.genero.join(", ")}<br>
+    <b>Autores:</b> ${comic.autores.join(", ")}
+`;
+
+comic.personajes.forEach(personaje => {
+    const miCard= document.createElement("div")
+    miCard.classList.add("personajes-item")
+    console.log(personaje.nombre)
+    miCard.innerHTML= `
+     <img class="personajes-imagen" src="${personaje.imagen}">
+                <p class="personajes-nombre">${personaje.nombre}</p>
+                `
+
+  personajesContainer.appendChild(miCard)
+    console.log(personaje.nombre)
+});
+
+/* ///////////////////////////////////////////////////////////////////////////////// */
+
+
+    comic.escenas.forEach( escena => {
+    const miCard= document.createElement("div");
+    miCard.classList.add("escenas-lista");
+    console.log(escena.nombre);
+    miCard.innerHTML= `
+    <div class="escena-item">
+                <img class="escena-imagen" src="${escena.image}">
+                <p class="escena-nombre">${escena.nombre}</p>
+            </div>`
+    listaEscenas.appendChild(miCard);
+    console.log(escena.nombre)
+});
+
+/* ///////////////////////////////////////////////////////////////////////////////// */
+
+/*   comic.escenas.forEach( escena => {
+    const miCard= document.createElement("div");
+    miCard.classList.add("escenas-lista");
+    console.log(escena.nombre);
+    miCard.innerHTML= `
+    <div class="escena-item">
+                <img class="escena-imagen" src="${escena.image}">
+                <p class="escena-nombre">${escena.nombre}</p>
+            </div>`
+    listaEscenas.appendChild(miCard);
+    console.log(escena.nombre)
+});
+ */
