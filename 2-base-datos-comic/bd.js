fdc05b87f312400af40e7e503c06d302c997d6b6
Luna Rivera <lunanicolle31@gmail.com>
2025-12-10 15:22:45 -0500

    Add files via upload

---
+++ 2-base-datos-comic/bd.js
@@ -0,0 +1,182 @@
+
+export let comic = {
+    "nombreComic": "Cenizas De Honor",
+    "sinopsis": "Un caballero siguiendo su destino",
+    "year": 2025,
+    "autores": ["Luna", "Sofia", "Alexis", "Miguel"],
+
+    "personajes":[
+        { "id": 1,
+          "nombre": "SR.LEON",
+          "imagen": "./img/Sr.León.jpg",
+          "descripcion": "El Sr. León es un valiente caballero que busca restaurar el honor de su familia después de una traición que lo dejó sin hogar y sin propósito. A lo largo de su viaje, enfrenta numerosos desafíos y enemigos, pero su determinación y sentido del deber lo impulsan a seguir adelante. Con la ayuda de aliados inesperados, el Sr. León lucha por recuperar su legado y proteger a los inocentes en un mundo lleno de peligros."
+        },
+        {
+        "id": 2,
+        "nombre": "ELVIRA",
+        "imagen": "./img/Elvira.jpg",
+        "descripcion": "Elvira es una astuta y valiente joven Elvira posee una inteligencia aguda y habilidades sorprendentes en el combate en inteligencia. A lo largo de la historia, enfrentándose a peligros y desafíos con valentía y determinación."
+        },
+        {"id": 3,
+        "nombre": "REY",
+        "imagen": "./img/Rey.jpg",
+        "descripcion": "El Rey es un monarca sabio y justo que gobierna su reino con benevolencia y firmeza. A lo largo de la historia, enfrenta desafíos tanto internos como externos. Con un fuerte sentido del deber hacia su pueblo, el Rey busca mantener la paz y la prosperidad en su reino, mientras navega por las complejidades del poder y la responsabilidad."
+        },
+        {"id": 4,
+        "nombre": "REINA",
+        "imagen": "./img/Reina.jpg",
+        "descripcion": "La Reina es una figura de gracia y fortaleza en el reino. Con una inteligencia aguda y un corazón compasivo, la Reina desempeña un papel crucial en la política y la diplomacia del reino. A lo largo de la historia, enfrenta desafíos que ponen a prueba su liderazgo y su capacidad para proteger a su pueblo, demostrando ser una aliada invaluable para el Rey y un símbolo de esperanza para todos."
+        },
+        {"id": 5,
+        "nombre": "SIRVIENTA",
+        "imagen": "./img/Sirvienta.jpg",
+        "descripcion": "La Sirvienta es una joven leal y trabajadora que sirve en la corte del reino. A lo largo de la historia, demuestra ser de gran valor para el protagonista, utilizando su ingenio y habilidades en momentos cruciales. A pesar de su posición humilde, la Sirvienta posee una gran fortaleza interior y un espíritu indomable que la impulsa a enfrentar desafíos."
+        },
+        {"id": 6,
+        "nombre": "BRUJAS DEL ALBA",
+        "imagen": "./img/brujas.jpg",
+        "descripcion": "Las Brujas del Alba son un grupo misterioso de mujeres que poseen conocimientos ancestrales y habilidades mágicas. A lo largo de la historia, juegan un papel crucial en el desarrollo del protagonista, ofreciendo guía y apoyo en momentos de necesidad. Aunque su lealtad puede ser ambigua, las Brujas del Alba son figuras poderosas que influyen en el destino."
+        },
+        {"id": 7,
+        "nombre": "PUEBLO",
+        "imagen": "./img/Gente.jpg",
+        "descripcion": "El Pueblo es una comunidad de personas comunes que viven bajo el gobierno del Rey y la Reina. A lo largo de la historia, representan la voz del pueblo y sus luchas diarias. Aunque a menudo son víctimas de las decisiones de los poderosos, el Pueblo demuestra una resiliencia y un espíritu colectivo que los impulsa a luchar por un futuro mejor."
+        },
+        {"id": 8,
+        "nombre": "CRISTOBAL COLON",
+        "imagen": "./img/Cristobal.jpg",
+        "descripcion": "Cristóbal Colón es un navegante y explorador audaz que sueña con descubrir nuevas tierras y rutas comerciales. A lo largo de la historia, su determinación y visión lo llevan a enfrentar numerosos desafíos en alta mar. Con un espíritu indomable y una fe inquebrantable en su misión, Cristóbal Colón se convierte en una figura histórica que cambia el curso del mundo."
+        },
+        {"id": 9,
+        "nombre": "CABALLEROS",
+        "imagen": "./img/Caballeros.jpg",
+        "descripcion": "Los Caballeros son guerreros valientes y leales que sirven al Rey y protegen el reino de amenazas externas. A lo largo de la historia, demuestran su destreza en el combate y su compromiso con el código de honor caballeresco. Aunque enfrentan numerosos desafíos, los Caballeros permanecen firmes en su deber, defendiendo a los inocentes y luchando por la justicia."
+        },
+        {"id": 10,
+        "nombre": "NINOVE",
+        "imagen": "./img/Ninove.jpg",
+        "descripcion": "Ninove es una hábil exploradora conocida por su conocimiento. A lo largo de la historia, Ninove se une al protagonista en su búsqueda, utilizando sus habilidades para superar obstáculos y enfrentar enemigos. Con un espíritu libre y una lealtad inquebrantable, Ninove se convierte en una aliada invaluable en la lucha por la justicia."
+        },
+        {"id": 11,
+        "nombre": "CONSEJERO",
+        "imagen": "./img/Consejero.jpg",
+        "descripcion": "El Consejero es un sabio y experimentado asesor del Rey, conocido por su astucia y conocimiento político. A lo largo de la historia, el Consejero desempeña un papel crucial en la toma de decisiones del reino, ofreciendo consejos estratégicos y orientación en tiempos de crisis. Aunque su lealtad puede ser cuestionada, el Consejero es una figura influyente que impacta el destino del reino."
+        },
+    ],
+
+    
+
+
+    "genero": ["Historia", "Fantasia", "Ciencia Ficción", "Acción"],
+    "portada": "./img/imagen1.png",
+    "escenas": [
+        {
+            "id": 1,
+            "nombre": "REINO",
+            "imagen": "./img/Reino.jpeg",
+            "video": "../img/Bosque.mp4",
+        },
+        {
+             "id": 2,
+            "nombre": "HABITACION REAL",
+            "imagen": "./img/HabitacionReal.jpeg",
+            "video": "../img/Bosque.mp4",
+        },
+        {
+            "id": 3,
+            "nombre": "BALCÓN REAL",
+            "imagen": "./img/BalconReal.jpeg",
+            "video": "../img/Bosque.mp4",
+        },
+        {
+            "id": 4,
+            "nombre": "SALA PINTURAS",
+            "imagen": "./img/SalaPinturas.jpeg",
+            "video": "../img/Bosque.mp4",
+        },
+        {
+            "id": 5,
+            "nombre": "SALON DE PIEDRA",
+            "imagen": "./img/SalaPiedra.jpeg",
+            "video": "../img/Bosque.mp4",
+        },
+        {
+            "id": 6,
+            "nombre": "LLANURAS",
+            "imagen": "./img/Llanuras.jpeg",
+            "video": "../img/Bosque.mp4",
+        },
+        {
+            "id": 7,
+            "nombre": "CAMPOS DE BATALLA",
+            "imagen": "./img/Guerra.jpeg",
+            "video": "../img/Bosque.mp4",
+        },
+        {
+            "id": 8,
+            "nombre": "CASTILLO BANQUETE",
+            "imagen": "./img/Banquete.jpeg",
+            "video": "../img/Bosque.mp4",
+        },
+        {
+            "id": 9,
+            "nombre": "PATIO CASTILLO",
+            "imagen": "./img/LLendose.png",
+            "video": "../img/Bosque.mp4",
+        },
+        {
+            "id": 10,
+            "nombre": "CAMINOS DESIERTOS",
+            "imagen": "./img/CaminoDesolado.jpeg",
+            "video": "../img/Bosque.mp4",
+        },
+        {
+            "id": 11,
+            "nombre": "BOSQUE OSCURO",
+            "imagen": "./img/Conversion.jpeg",
+            "video": "../img/Bosque.mp4",
+        },
+        {
+            "id": 12,
+            "nombre": "CUEVA ANCESTRAL",
+            "imagen": "./img/Entrega.jpeg",
+            "video": "../img/Bosque.mp4",
+        },
+        {
+            "id": 13,
+            "nombre": "BATALLA MEDIEVAL",
+            "imagen": "./img/GuerraMedieval.jpeg",
+            "video": "../img/Bosque.mp4",
+        },
+        {
+            "id": 14,
+            "nombre": "TABLERO DE AJEDREZ",
+            "imagen": "./img/GuerraAjedrez.jpeg",
+            "video": "../img/Bosque.mp4",
+        },
+        {
+            "id": 15,
+            "nombre": "ALDEA EN RUINAS",
+            "imagen": "./img/Maldicion.jpeg",
+            "video": "../img/Bosque.mp4",
+        },
+        {
+            "id": 16,
+            "nombre": "PUERTO DE ESPAÑA",
+            "imagen": "./img/España.jpeg", 
+            "video": "../img/Bosque.mp4",  
+        },
+        {
+            "id": 17,
+            "nombre": "SELVA AMERICANA",
+            "imagen": "./img/Pisando.jpeg",
+            "video": "../img/Bosque.mp4",
+        },
+        {
+            "id": 18,
+            "nombre": "INGLATERRA",
+            "imagen": "./img/Muerte.jpeg",
+            "video": "../img/Bosque.mp4",
+        }
+    ]
+}
